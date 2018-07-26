7d33734c0dbcca2ff44d1b3f4ce0fa0fb1f1d2a8
Michael Yu <m.yu8088@gmail.com>
2018-07-26 17:37:04 -0500

    add some more tests

--- __test__/api/teams.test.js
+++ __test__/api/teams.test.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 import app from '../../server/app'
 import Tournament from '../../server/models/Tournament'
+import Team from '../../server/models/Team'
 import { TEST_URL } from '../config'
 
 jest.mock('../../server/passport/auth', () => ({
@@ -10,13 +11,13 @@ jest.mock('../../server/passport/auth', () => ({
 	needsGroup: jest.fn().mockImplementation(() => (req, res, next) => next()),
 }))
 
-describe('GET /users', () => {
-	let tournament
+describe('Teams API Tests', () => {
+	let testTournament
 
 	beforeAll(async () => {
 		await mongoose.connect(TEST_URL)
-		tournament = await Tournament.create({
-			name: 'test tournament',
+		testTournament = await Tournament.create({
+			name: 'test tournament for teams',
 			date: Date.now(),
 			state: 'TX',
 			city: 'Austin',
@@ -29,25 +30,82 @@ describe('GET /users', () => {
 		await app.close()
 	})
 
-	test('Returns an empty list of teams for a new tournament', async () => {
-		const response = await request(app).get(`/tournaments/${tournament._id}/teams`)
-		expect(response.statusCode).toBe(200)
-		expect(response.body.length).toBe(0)
+	describe('GET /tournaments/:tournamentId/teams', () => {
+		test('Returns an empty list of teams for a new tournament', async () => {
+			const response = await request(app).get(`/tournaments/${testTournament.id}/teams`)
+			expect(response.statusCode).toBe(200)
+			expect(response.body.length).toBe(0)
+		})
 	})
 
-	test('Can successfully create a single team', async () => {
-		const testTeam = {
-			school: 'test school',
-			division: 'B',
-			teamNumber: 1,
-			tournament: tournament._id,
-		}
-		const response = await request(app)
-			.post(`/tournaments/${tournament._id}/teams`)
-			.send(testTeam)
+	describe('POST /tournaments/:tournamentId/teams', () => {
+		let url
 
-		expect(response.statusCode).toBe(201)
-		expect(response.body.length).toBe(1)
-		expect(response.body[0].school).toBe('test school')
+		beforeAll(() => {
+			url = `/tournaments/${testTournament.id}/teams`
+		})
+
+		afterEach(async () => {
+			await Team.deleteMany().exec()
+		})
+
+		test('Successfully creates a single team', async () => {
+			const testTeam = {
+				school: 'test school',
+				division: 'B',
+				teamNumber: 1,
+				tournament: testTournament.id,
+			}
+			const response = await request(app)
+				.post(url)
+				.send(testTeam)
+
+			expect(response.statusCode).toBe(201)
+			expect(response.body.length).toBe(1)
+
+			const returnedTournament = response.body[0]
+			expect(returnedTournament).toHaveProperty('school', 'test school')
+			expect(returnedTournament).toHaveProperty('tournament', testTournament.id)
+		})
+
+		test('Successfully creates multiple teams at once', async () => {
+			const teams = [
+				{
+					school: 'test school 1',
+					division: 'B',
+					teamNumber: 1,
+					tournament: testTournament.id,
+				},
+				{
+					school: 'test school 2',
+					division: 'C',
+					teamNumber: 1,
+					tournament: testTournament.id,
+				},
+			]
+
+			const response = await request(app)
+				.post(url)
+				.send(teams)
+
+			expect(response.statusCode).toBe(201)
+			expect(response.body.length).toBe(2)
+		})
+
+		test('Errors when creating with missing/bad params', async () => {
+			const badTeam = {
+				school: null,
+				tournament: null,
+				division: 'D',
+				teamNumber: 1,
+			}
+
+			const response = await request(app)
+				.post(url)
+				.send(badTeam)
+
+			expect(response.statusCode).toBe(400)
+			expect(response.body).toHaveProperty('errors')
+		})
 	})
 })
--- server/routes/errorHandler.js
+++ server/routes/errorHandler.js
@@ -3,7 +3,7 @@ import { DuplicateError, ValidationError, InternalServerError } from '../errors'
 const errorHandler = (err, req, res, next) => {
 	// We want to print out error to console when in developing
 	// eslint-disable-next-line
-	if (process.env.NODE_ENV !== 'production' || process.env.LOG_ERRORS) console.log(err.stack)
+	if (process.env.NODE_ENV === 'development' || process.env.LOG_ERRORS) console.log(err.stack)
 	if (!err) return next()
 
 	if (err.name === 'MongoError' && err.code === 11000) {
