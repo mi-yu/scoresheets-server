8a1dcc17dfd44cc93e562ba49687dde97f9f846e
mi-yu <michael.yu@wpengine.com>
2018-08-29 14:22:21 -0500

    refactored seeds

--- __test__/api/users.test.js
+++ __test__/api/users.test.js
@@ -2,12 +2,16 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 import app from '../../server/app'
 import { TEST_URL } from '../config'
+import { ensureAuthenticated, needsGroup } from '../../server/passport/auth'
+import { UnauthorizedError, ForbiddenError } from '../../server/errors'
+import users from '../seeds/users.seed.json'
 
 jest.mock('../../server/passport/auth', () => ({
 	ensureAuthenticated: jest.fn().mockImplementation((req, res, next) => next()),
 	permitUnauthenticated: jest.fn().mockImplementation((req, res, next) => next()),
 	needsGroup: jest.fn().mockImplementation(() => (req, res, next) => next()),
 }))
+
 
 describe('GET /users', () => {
 	beforeAll(async () => {
@@ -19,8 +23,22 @@ describe('GET /users', () => {
 		await app.close()
 	})
 
-	test('should return 200', async () => {
+	it('should return 200 when authenticated and user is admin', async () => {
+		const response = await request(app).get('/users')
+
+		expect(response.statusCode).toBe(200)
+		expect(response.body.sort((a, b) => a.email.localCompare(b.email))).toMatchObject(users)
+	})
+
+	it('should return 403 when authenticated and user is not admin', async () => {
+		needsGroup.mockImplementation(() => (req, res, next) => next(new ForbiddenError()))
 		const response = await request(app).get('/users')
 		expect(response.statusCode).toBe(200)
+	})
+
+	it('should return 401 when unauthenticated', async () => {
+		ensureAuthenticated.mockImplementation((req, res, next) => next(new UnauthorizedError()))
+		const response = await request(app).get('/users')
+		expect(response.statusCode).toBe(401)
 	})
 })
